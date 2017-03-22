76f2ba0c1685531e8414f54f1611cbc36e57d0f8
Sigalas Alexandros <alxarch@gmail.com>
2017-03-22 12:10:37 +0200

    Add Retries and Stats

--- batch.go
+++ batch.go
@@ -2,6 +2,8 @@ package batch
 
 import (
 	"context"
+	"errors"
+	"sync/atomic"
 	"time"
 )
 
@@ -16,67 +18,150 @@ func (df DrainFunc) Drain(ctx context.Context, batch []interface{}) error {
 }
 
 type Queue struct {
-	Interval time.Duration
 	Size     int
+	Interval time.Duration
 	Drainer  Drainer
+	Retries  int
 
 	queue chan interface{}
+	stats struct {
+		items      uint64
+		flush_size uint64
+		flush_tick uint64
+		flush_err  uint64
+	}
+	up int64
 }
 
-const DefaultSize = 1000
-const DefaultInterval = time.Second
+func (s *Queue) IsUp() bool {
+	return atomic.LoadInt64(&s.up) > 0
+}
 
-func (s *Queue) Run(ctx context.Context) context.Context {
+func (s *Queue) Stats() (total, flush_size, flush_tick, flush_err uint64) {
+	return atomic.LoadUint64(&s.stats.items),
+		atomic.LoadUint64(&s.stats.flush_size),
+		atomic.LoadUint64(&s.stats.flush_tick),
+		atomic.LoadUint64(&s.stats.flush_err)
+}
+
+func (s *Queue) MustRun(ctx context.Context) {
+	if err := s.Run(ctx); err != nil {
+		panic(err)
+	}
+}
+
+var (
+	NoDrainerError = errors.New("No Drainer in Queue")
+)
+
+func (s *Queue) Flush(ctx context.Context, batch []interface{}) (err error) {
+	if nil == s.Drainer {
+		return NoDrainerError
+	}
+	r := 0
+	if s.Retries > 0 {
+		r = s.Retries
+	}
+	for ; r >= 0; r-- {
+		if err = s.Drainer.Drain(ctx, batch); err == nil {
+			break
+		} else {
+			atomic.AddUint64(&s.stats.flush_err, 1)
+		}
+	}
+	return
+}
+
+var (
+	AlreadyRunningError = errors.New("Queue already running.")
+)
+
+const (
+	DefaultInterval = time.Second
+	DefaultSize     = 1000
+)
+
+func (s *Queue) Run(ctx context.Context) error {
+	if n := atomic.AddInt64(&s.up, 1); n > 1 {
+		return AlreadyRunningError
+	}
+	interval := DefaultInterval
+	if s.Interval >= 0 {
+		interval = s.Interval
+	}
+	size := DefaultSize
+	if s.Size > 0 {
+		size = s.Size
+	}
+	tick := time.NewTicker(interval)
 	if nil == ctx {
 		ctx = context.Background()
 	}
-	size := s.Size
-	if size <= 0 {
-		size = DefaultSize
-	}
+
 	s.queue = make(chan interface{}, 2*size)
-	if s.Interval <= 0 {
-		s.Interval = DefaultInterval
-	}
-	tick := time.NewTicker(s.Interval)
+	errors := make(chan error, 1)
 	batch := make([]interface{}, 0, size)
-	drain := func() {
-		if len(batch) > 0 {
-			// Non-blocking drain
-			if s.Drainer != nil {
-				go func(b []interface{}) {
-					s.Drainer.Drain(ctx, b)
-				}(batch)
+	flush := func() {
+		// Non-blocking drain
+		go func(batch []interface{}) {
+			if err := s.Flush(ctx, batch); err != nil {
+				errors <- err
 			}
-			batch = make([]interface{}, 0, size)
-		}
+		}(batch)
+		batch = make([]interface{}, 0, s.Size)
 	}
 
 	go func() {
-		defer close(s.queue)
-		defer tick.Stop()
-		defer drain()
+		// Drain last batch if needed
+		defer func() {
+			s.up = 0
+			close(s.queue)
+			close(errors)
+			tick.Stop()
+			if len(batch) > 0 {
+				flush()
+			}
+		}()
 		for {
 			select {
 			case <-ctx.Done():
 				return
+			case _ = <-errors:
+				s.stats.flush_err++
 			case <-tick.C:
-				drain()
+				if len(batch) > 0 {
+					flush()
+					s.stats.flush_tick++
+				}
 			case x := <-s.queue:
 				if len(batch) == cap(batch) {
-					drain()
+					flush()
+					s.stats.flush_size++
 				}
 				batch = append(batch, x)
+				s.stats.items++
 			}
 		}
+
 	}()
-	return ctx
+
+	return nil
 }
 
-func (s *Queue) Add(x interface{}) {
-	if nil == s.queue {
-		s.Run(nil)
-	}
+var (
+	NotRunningError = errors.New("Queue is not running")
+)
 
+func (s *Queue) MustAdd(x interface{}) {
+	if err := s.Add(x); err != nil {
+		panic(err)
+	}
+}
+
+func (s *Queue) Add(x interface{}) error {
+	if nil == s.queue {
+		return NotRunningError
+	}
 	s.queue <- x
+	return nil
 }
--- batch_test.go
+++ batch_test.go
@@ -2,12 +2,37 @@ package batch_test
 
 import (
 	"context"
+	"errors"
+	"sync/atomic"
 	"testing"
 	"time"
 
 	"github.com/alxarch/go-batch"
 )
 
+func Test_Retries(t *testing.T) {
+	n := int64(0)
+	done := make(chan struct{})
+	q := &batch.Queue{
+		Size:     2,
+		Retries:  2,
+		Interval: 5 * time.Second,
+		Drainer: batch.DrainFunc(func(ctx context.Context, b []interface{}) error {
+			if atomic.AddInt64(&n, 1) > 2 {
+				close(done)
+				return nil
+			}
+			return errors.New("ERR")
+		}),
+	}
+	q.MustRun(nil)
+	q.Add(1)
+	q.Add(1)
+	<-done
+	if n != 3 {
+		t.Error("No retries")
+	}
+}
 func Test_Queue(t *testing.T) {
 
 	done := make(chan []interface{}, 1)
@@ -19,23 +44,42 @@ func Test_Queue(t *testing.T) {
 			return nil
 		}),
 	}
-	ctx, cancel := context.WithCancel(context.Background())
-	c := q.Run(ctx)
-	if ctx != c {
-		t.Error("Not same c")
+	err := q.Add("foo")
+	if err != batch.NotRunningError {
+		t.Error("Invalid not running error")
 	}
-	q.Add("a")
+	ctx, cancel := context.WithCancel(context.Background())
+	q.MustRun(ctx)
+	if err := q.Run(nil); err != batch.AlreadyRunningError {
+		t.Error("Invalid running error")
+
+	}
+
+	q.MustAdd("a")
 	q.Add("b")
 	q.Add("c")
 	b := <-done
 	if len(b) != 2 {
-		t.Error("Invalid batch length")
+		t.Error("Invalid batch length %d", len(b))
 	}
 	if v, ok := b[0].(string); !ok || v != "a" {
 		t.Error("Invalid batch elem 0")
 	}
 	if v, ok := b[1].(string); !ok || v != "b" {
 		t.Error("Invalid batch elem 1 %s", v)
+	}
+	items, fsize, ftick, ferr := q.Stats()
+	if items != 3 {
+		t.Error("Invalid stats items %d", items)
+	}
+	if fsize != 1 {
+		t.Error("Invalid stats fsize")
+	}
+	if ftick != 0 {
+		t.Error("Invalid stats ftick")
+	}
+	if ferr != 0 {
+		t.Error("Invalid stats ferr")
 	}
 
 	cancel()
